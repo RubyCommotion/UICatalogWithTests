99ec36d607fdf875efed7ef1fccd38ef8ba17288
Dennis Major <dennismajor1@gmail.com>
2014-02-13 21:33:19 -0500

    Further tests have been added to cover rc_alerts_view_controller.rb
    - tests yet to be completed are for thaw UIAlert four objects

--- spec/controllers/rc_alerts_view_controller_spec.rb
+++ spec/controllers/rc_alerts_view_controller_spec.rb
@@ -1,4 +1,4 @@
-describe "\nTests RcAlertsViewController attr_accessor,constants and view title. " do
+describe "\nAn Instance of the RcAlertsViewController " do
   tests RcAlertsViewController
 
   before do
@@ -9,39 +9,72 @@ describe "\nTests RcAlertsViewController attr_accessor,constants and view title.
   end
 
 
-  it 'should respond to attr_accessor \'dialog_simple_action_obj\'' do
-    controller.respond_to?(:dialog_simple_action_obj).should == true
-  end
-
-  it 'should set the view\'s title' do
+  it 'should set its view title to \'Alerts\'' do
     controller.title.should == 'Alerts'
   end
 
-  it 'should have a constant ALERT_CELL_ID and SOURCE_CELL_ID with values of \'AlertCellID\' and \'SourceCellID\'' do
-    RcAlertsViewController::ALERT_CELL_ID.should == 'AlertCellID'
-    RcAlertsViewController::SOURCE_CELL_ID.should == 'SourceCellID'
+  it 'should have two constants ALERT_CELL_ID and SOURCE_CELL_ID' do
+    RcAlertsViewController::ALERT_CELL_ID.should.not.be == nil
+    RcAlertsViewController::SOURCE_CELL_ID.should.not == nil
   end
 
-  it 'should have 7 @data_source_array rows' do
+  it 'should have a @data_source_array consisting of 7 rows' do
     controller.instance_variable_get('@data_source_array').count.should == 7
   end
 
+  it 'should respond to three UIActionSheet methods \'dialog_simple_action\', \'dialog_ok_cancel_action\' \'dialog_other_action\' ' do
+    controller.respond_to?(:dialog_simple_action).should == true
+    controller.respond_to?(:dialog_ok_cancel_action).should == true
+    controller.respond_to?(:dialog_other_action).should == true
+  end
 
-    describe "Tests UIActionSheet\'s three alert dialogs\n" do
+  it 'should respond to four UIAlertView methods \'alert_simple_action\', \'alert_ok_cancel_action\' \'alert_other_action\',
+     \'alert_secure_text_action\' ' do
+    controller.respond_to?(:alert_simple_action).should == true
+    controller.respond_to?(:alert_ok_cancel_action).should == true
+    controller.respond_to?(:alert_other_action).should == true
+    controller.respond_to?(:alert_secure_text_action).should == true
+  end
 
-      it 'should respond to ActionSheet methods \'dialog_simple_action\', \'dialog_ok_cancel_action\' \'dialog_other_action\' ' do
-        controller.respond_to?(:dialog_simple_action).should == true
-        controller.respond_to?(:dialog_ok_cancel_action).should == true
-        controller.respond_to?(:dialog_other_action).should == true
-      end
+  it 'should have a table that is displaying one or more visible cells.' do
+    controller.tableView.visibleCells.should.not.be.empty
+  end
 
-      it 'should have a dialog simple action with title of UIActionSheet Alert With OK Button' do
+
+    describe "The UIActionSheet\'s three alert dialogs\n" do
+
+      it 'should have a \'def dialog_simple_action\' that returns an object with defined states for title, delegate,
+          destructiveButtonTitle, and otherButtonTitles'   do
         controller.dialog_simple_action_obj.title.should == 'UIActionSheet Alert With OK Button'
+        controller.dialog_simple_action_obj.delegate.should == controller
+        controller.dialog_simple_action_obj.cancelButtonIndex.should == -1 # does not exist
+        controller.dialog_simple_action_obj.destructiveButtonIndex.should == 0
+        controller.dialog_simple_action_obj.buttonTitleAtIndex(0).should == 'OK' # destructiveButtonIndex
+        controller.dialog_simple_action_obj.firstOtherButtonIndex.should == -1
       end
 
-      it 'should have a dialog action with title of UIActionSheet Alert With Cancel and OK' do
+      it 'should have a \'def dialog_ok_cancel_action\' that returns an object with defined states for title, delegate,
+          cancelButtonTitle, destructiveButtonTitle, and otherButtonTitles'   do
         controller.dialog_ok_cancel_action_obj.title.should == 'UIActionSheet Alert With Cancel and OK'
+        controller.dialog_ok_cancel_action_obj.delegate.should == controller
+        controller.dialog_ok_cancel_action_obj.cancelButtonIndex.should == 1
+        controller.dialog_ok_cancel_action_obj.destructiveButtonIndex.should == 0
+        controller.dialog_ok_cancel_action_obj.buttonTitleAtIndex(0).should == 'OK'
+        controller.dialog_ok_cancel_action_obj.firstOtherButtonIndex.should == -1
       end
+
+
+      it 'should have a \'def dialog_other_action\' that returns an object with defined states for title, delegate,
+          cancelButtonTitle, destructiveButton, otherButton indices and otherButtonTitles'  do
+        controller.dialog_other_action_obj.title.should == 'UIActionSheet Custom Alert'
+        controller.dialog_other_action_obj.delegate.should == controller
+        controller.dialog_other_action_obj.cancelButtonIndex.should == -1
+        controller.dialog_other_action_obj.destructiveButtonIndex.should == 1
+        controller.dialog_other_action_obj.firstOtherButtonIndex.should == 0
+        controller.dialog_other_action_obj.buttonTitleAtIndex(0).should == 'Button1'
+        controller.dialog_other_action_obj.buttonTitleAtIndex(1).should == 'Button2'
+      end
+
 
     end
 end
